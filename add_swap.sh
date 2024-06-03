d3349b896a25fbd74c40c6d6461b7502513f6c9c
www <yooyu@msn.com>
2024-06-03 20:54:21 +0800

    Update add_swap.sh

--- add_swap.sh
+++ add_swap.sh
@@ -1,18 +1,24 @@
 #!/bin/bash
 
+# 检查是否以root用户运行脚本
+if [[ $EUID -ne 0 ]]; then
+   echo "本脚本需要以root用户权限运行" 
+   exit 1
+fi
+
 # 创建交换文件
-sudo fallocate -l 5G /swapfile
+fallocate -l 5G /swapfile
 
 # 设置文件权限
-sudo chmod 600 /swapfile
+chmod 600 /swapfile
 
 # 将文件转换为交换文件
-sudo mkswap /swapfile
+mkswap /swapfile
 
 # 启用交换文件
-sudo swapon /swapfile
+swapon /swapfile
 
 # 将交换文件添加到 /etc/fstab 中
-echo '/swapfile   none    swap    sw    0   0' | sudo tee -a /etc/fstab
+echo '/swapfile none swap sw 0 0' | tee -a /etc/fstab
 
 echo "5GB 虚拟内存已成功添加！"
