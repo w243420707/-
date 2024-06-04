647e7dd8af1f3ad2814c427745dc109901650759
www <yooyu@msn.com>
2024-06-04 22:24:50 +0800

    Update hy-au-l4ehusajhz18.sh

--- DDNS/hy-au-l4ehusajhz18.sh
+++ DDNS/hy-au-l4ehusajhz18.sh
@@ -3,123 +3,47 @@ set -o errexit
 set -o nounset
 set -o pipefail
 
-# Automatically update your CloudFlare DNS record to the IP, Dynamic DNS
-# Can retrieve cloudflare Domain id and list zone's, because, lazy
+# 默认配置
+CFKEY=e80a9bfb256d5d060aa8a4f55a7da43fdf135  # API 密钥
+CFUSER=yooyu@msn.com  # 用户名
+CFZONE_NAME=fly64jfgwhale.xyz  # 区域名称
+CFRECORD_NAME=hy-au-l4ehusajhz18.fly64jfgwhale.xyz  # 要更新的主机名
+CFRECORD_TYPE=A  # 记录类型，A(IPv4)或AAAA(IPv6)
+CFTTL=120  # Cloudflare 记录的 TTL
+FORCE=true  # 忽略本地文件，反正要更新 IP
 
-# Place at:
-# curl https://raw.githubusercontent.com/yulewang/cloudflare-api-v4-ddns/master/cf-v4-ddns.sh > /usr/local/bin/cf-ddns.sh && chmod +x /usr/local/bin/cf-ddns.sh
-# run `crontab -e` and add next line:
-# */1 * * * * /usr/local/bin/cf-ddns.sh >/dev/null 2>&1
-# or you need log:
-# */1 * * * * /usr/local/bin/cf-ddns.sh >> /var/log/cf-ddns.log 2>&1
+WANIPSITE="http://ipv4.icanhazip.com"  # 获取 WAN IP 的站点
 
-
-# Usage:
-# cf-ddns.sh -k cloudflare-api-key \
-#            -u user@example.com \
-#            -h host.example.com \     # fqdn of the record you want to update
-#            -z example.com \          # will show you all zones if forgot, but you need this
-#            -t A|AAAA                 # specify ipv4/ipv6, default: ipv4
-
-# Optional flags:
-#            -f false|true \           # force dns update, disregard local stored ip
-
-# default config
-
-# API key, see https://www.cloudflare.com/a/account/my-account,
-# incorrect api-key results in E_UNAUTH error
-CFKEY=e80a9bfb256d5d060aa8a4f55a7da43fdf135
-
-# Username, eg: user@example.com
-CFUSER=yooyu@msn.com
-
-# Zone name, eg: example.com
-CFZONE_NAME=fly64jfgwhale.xyz
-
-# Hostname to update, eg: homeserver.example.com
-CFRECORD_NAME=hy-au-l4ehusajhz18.fly64jfgwhale.xyz
-
-# Record type, A(IPv4)|AAAA(IPv6), default IPv4
-CFRECORD_TYPE=A
-
-# Cloudflare TTL for record, between 120 and 86400 seconds
-CFTTL=120
-
-# Ignore local file, update ip anyway
-FORCE=ture
-
-WANIPSITE="http://ipv4.icanhazip.com"
-
-# Site to retrieve WAN ip, other examples are: bot.whatismyipaddress.com, https://api.ipify.org/ ...
-if [ "$CFRECORD_TYPE" = "A" ]; then
-  :
-elif [ "$CFRECORD_TYPE" = "AAAA" ]; then
+# 如果记录类型为 AAAA，则更改 WANIPSITE
+if [ "$CFRECORD_TYPE" = "AAAA" ]; then
   WANIPSITE="http://ipv6.icanhazip.com"
-else
-  echo "$CFRECORD_TYPE specified is invalid, CFRECORD_TYPE can only be A(for IPv4)|AAAA(for IPv6)"
+elif [ "$CFRECORD_TYPE" != "A" ]; then
+  echo "$CFRECORD_TYPE 指定无效，CFRECORD_TYPE 只能是 A（用于 IPv4）或 AAAA（用于 IPv6）"
   exit 2
 fi
 
-# get parameter
-while getopts k:u:h:z:t:f: opts; do
-  case ${opts} in
-    k) CFKEY=${OPTARG} ;;
-    u) CFUSER=${OPTARG} ;;
-    h) CFRECORD_NAME=${OPTARG} ;;
-    z) CFZONE_NAME=${OPTARG} ;;
-    t) CFRECORD_TYPE=${OPTARG} ;;
-    f) FORCE=${OPTARG} ;;
-  esac
-done
-
-# If required settings are missing just exit
-if [ "$CFKEY" = "" ]; then
-  echo "Missing api-key, get at: https://www.cloudflare.com/a/account/my-account"
-  echo "and save in ${0} or using the -k flag"
-  exit 2
-fi
-if [ "$CFUSER" = "" ]; then
-  echo "Missing username, probably your email-address"
-  echo "and save in ${0} or using the -u flag"
-  exit 2
-fi
-if [ "$CFRECORD_NAME" = "" ]; then 
-  echo "Missing hostname, what host do you want to update?"
-  echo "save in ${0} or using the -h flag"
-  exit 2
-fi
-
-# If the hostname is not a FQDN
-if [ "$CFRECORD_NAME" != "$CFZONE_NAME" ] && ! [ -z "${CFRECORD_NAME##*$CFZONE_NAME}" ]; then
-  CFRECORD_NAME="$CFRECORD_NAME.$CFZONE_NAME"
-  echo " => Hostname is not a FQDN, assuming $CFRECORD_NAME"
-fi
-
-# Get current and old WAN ip
-WAN_IP=`curl -s ${WANIPSITE}`
+# 获取当前和旧的 WAN IP
+WAN_IP=$(curl -s ${WANIPSITE})
 WAN_IP_FILE=$HOME/.cf-wan_ip_$CFRECORD_NAME.txt
 if [ -f $WAN_IP_FILE ]; then
-  OLD_WAN_IP=`cat $WAN_IP_FILE`
+  OLD_WAN_IP=$(cat $WAN_IP_FILE)
 else
-  echo "No file, need IP"
   OLD_WAN_IP=""
 fi
 
-# If WAN IP is unchanged an not -f flag, exit here
+# 如果 WAN IP 未更改且未使用 FORCE 标志，则在此退出
 if [ "$WAN_IP" = "$OLD_WAN_IP" ] && [ "$FORCE" = false ]; then
-  echo "WAN IP Unchanged, to update anyway use flag -f true"
   exit 0
 fi
 
-# Get zone_identifier & record_identifier
+# 获取 zone_identifier 和 record_identifier
 ID_FILE=$HOME/.cf-id_$CFRECORD_NAME.txt
-if [ -f $ID_FILE ] && [ $(wc -l $ID_FILE | cut -d " " -f 1) == 4 ] \
-  && [ "$(sed -n '3,1p' "$ID_FILE")" == "$CFZONE_NAME" ] \
-  && [ "$(sed -n '4,1p' "$ID_FILE")" == "$CFRECORD_NAME" ]; then
-    CFZONE_ID=$(sed -n '1,1p' "$ID_FILE")
-    CFRECORD_ID=$(sed -n '2,1p' "$ID_FILE")
+if [ -f $ID_FILE ] && [ $(wc -l < $ID_FILE) -eq 4 ] \
+  && [ "$(sed -n '3p' "$ID_FILE")" == "$CFZONE_NAME" ] \
+  && [ "$(sed -n '4p' "$ID_FILE")" == "$CFRECORD_NAME" ]; then
+    CFZONE_ID=$(sed -n '1p' "$ID_FILE")
+    CFRECORD_ID=$(sed -n '2p' "$ID_FILE")
 else
-    echo "Updating zone_identifier & record_identifier"
     CFZONE_ID=$(curl -s -X GET "https://api.cloudflare.com/client/v4/zones?name=$CFZONE_NAME" -H "X-Auth-Email: $CFUSER" -H "X-Auth-Key: $CFKEY" -H "Content-Type: application/json" | grep -Po '(?<="id":")[^"]*' | head -1 )
     CFRECORD_ID=$(curl -s -X GET "https://api.cloudflare.com/client/v4/zones/$CFZONE_ID/dns_records?name=$CFRECORD_NAME" -H "X-Auth-Email: $CFUSER" -H "X-Auth-Key: $CFKEY" -H "Content-Type: application/json"  | grep -Po '(?<="id":")[^"]*' | head -1 )
     echo "$CFZONE_ID" > $ID_FILE
@@ -128,21 +52,16 @@ else
     echo "$CFRECORD_NAME" >> $ID_FILE
 fi
 
-# If WAN is changed, update cloudflare
-echo "Updating DNS to $WAN_IP"
-
+# 更新 Cloudflare
 RESPONSE=$(curl -s -X PUT "https://api.cloudflare.com/client/v4/zones/$CFZONE_ID/dns_records/$CFRECORD_ID" \
   -H "X-Auth-Email: $CFUSER" \
   -H "X-Auth-Key: $CFKEY" \
   -H "Content-Type: application/json" \
   --data "{\"id\":\"$CFZONE_ID\",\"type\":\"$CFRECORD_TYPE\",\"name\":\"$CFRECORD_NAME\",\"content\":\"$WAN_IP\", \"ttl\":$CFTTL}")
 
-if [ "$RESPONSE" != "${RESPONSE%success*}" ] && [ "$(echo $RESPONSE | grep "\"success\":true")" != "" ]; then
-  echo "Updated succesfuly!"
+if [[ $RESPONSE == *"\"success\":true"* ]]; then
   echo $WAN_IP > $WAN_IP_FILE
-  exit
 else
-  echo 'Something went wrong :('
-  echo "Response: $RESPONSE"
+  echo "更新失败: $RESPONSE"
   exit 1
 fi
