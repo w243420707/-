0cbface71f999e3b8f634242d4d472a3a12eda92
www <yooyu@msn.com>
2024-12-16 19:05:38 +0800

    Update setup_cron.sh

--- setup_cron.sh
+++ setup_cron.sh
@@ -6,10 +6,10 @@ if ! command -v crontab &> /dev/null; then
     apk add --no-cache dcron
 fi
 
-# 设置每分钟执行一次 "rc-service V2bX start"
-(crontab -l 2>/dev/null; echo "* * * * * rc-service V2bX start") | crontab -
+# 设置每十分钟执行一次 "rc-service V2bX start"
+(crontab -l 2>/dev/null | grep -v "rc-service V2bX start"; echo "*/10 * * * * rc-service V2bX start") | crontab -
 
 # 启动cron服务
 service crond start
 
-echo "定时任务已设置，每分钟执行一次 'rc-service V2bX start'"
+echo "定时任务已设置，每十分钟执行一次 'rc-service V2bX start'"
